94ef18047ae7f1cda09468789d266cce3f1579aa
Clauderic Demers <clauderic@peoplelikeus.ca>
2015-10-28 16:17:56 -0400

    Don't tamper with cordova status bar color plugin

--- src/android/StatusBarColor.java
+++ src/android/StatusBarColor.java
@@ -26,10 +26,6 @@ public class StatusBarColor extends CordovaPlugin {
                     cordova.getActivity().setTaskDescription(new ActivityManager.TaskDescription(description.getLabel(), description.getIcon(), color));
                 }
             }
-            Window window = cordova.getActivity().getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(color);
         }
     }
 }
